14181988c515683b88f8562544aa4387ff7700d5
Vanessa Bautista <vanessawebdev@gmail.com>
2021-11-08 12:12:11 -0800

    created a filter methodfor monsters list and added styling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { CardList } from "./components/card-list/card-list.component.jsx";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asc1",
-        },
-        {
-          name: "Dracula",
-          id: "asr2",
-        },
-        {
-          name: "Zombie",
-          id: "as1w",
-        },
-      ],
+      monsters: [],
+      searchField: "",
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => this.setState({ monsters: users }))
+    );
+  }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) =>
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            )
+          }
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
